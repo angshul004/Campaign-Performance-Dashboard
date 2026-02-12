6b4973374dd702e349291039fa23a85cc1bb5664
Angshul <angshul.sana.2004@gmail.com>
2026-02-12 23:24:12 +0530

    auto db create

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from config import Config
@@ -7,6 +9,7 @@ from .extensions import db
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config)
+    os.makedirs(app.instance_path, exist_ok=True)
 
     db.init_app(app)
 
